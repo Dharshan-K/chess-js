8f3338471956e39de5edbf5dc325e32fc689d4bf
Dharshan K <kdharshanprog@gmail.com>
2023-06-07 21:09:56 +0530

    chess board done

---
+++ index.js
@@ -0,0 +1,115 @@
+/** @format */
+import { rook, knight, bishop, queen, king, pawn } from "./pices.js";
+const gameBoard = document.querySelector("#gameboard");
+const playerDisplay = document.querySelector("#player");
+const infoDisplay = document.querySelector("#info-display");
+
+const Pieces = [
+  rook,
+  knight,
+  bishop,
+  queen,
+  king,
+  bishop,
+  knight,
+  rook,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  rook,
+  knight,
+  bishop,
+  queen,
+  king,
+  bishop,
+  knight,
+  rook,
+];
+
+function createBoard() {
+  Pieces.forEach((startPiece, i) => {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.innerHTML = startPiece;
+    square.firstChild?.setAttribute("draggable", true);
+    square.setAttribute("square-id", i);
+    const row = Math.floor((63 - i) / 8) + 1;
+    if (row % 2 === 0) {
+      console.log("white", row, i);
+      square.classList.add(i % 2 === 0 ? "orange" : "blue");
+    } else {
+      console.log("blue", row, i);
+      square.classList.add(i % 2 === 0 ? "blue" : "orange");
+    }
+    if (i <= 15) {
+      square.firstChild.firstChild.classList.add("white-piece");
+    }
+    gameBoard.append(square);
+  });
+}
+
+createBoard();
+
+const allSquare = document.querySelectorAll("#gameboard .square");
+
+allSquare.forEach((square) => {
+  square.addEventListener("dragstart", dragstart);
+});
+
+let startPositionId;
+let draggedElement;
+
+function dragstart(e) {
+  startPositionId = e.target.parentNode.getAttribute("square-id");
+  draggedElement = e.target;
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
